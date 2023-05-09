28e0dd000a009042ee96504e48ca902b2d198d73
NIKOLELACUNZA <101419628+NIKOLELACUNZA@users.noreply.github.com>
2023-05-08 19:57:08 -0500

    Add files via upload

---
+++ prueba server less/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace prueba_server_less.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
